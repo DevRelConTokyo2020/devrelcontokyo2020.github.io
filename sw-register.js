e03ece6618eba068e03e6256533725d10cc914a7
Atsushi <atsushi@moongift.jp>
2020-01-09 08:59:04 +0900

    Updates

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1578471268').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1578527893').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
